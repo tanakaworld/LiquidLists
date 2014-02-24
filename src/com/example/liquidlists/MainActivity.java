35a9cedd54937188909f2207c049f984e3f0d51c
yutaro <y.tanaka.kcbbf@gmail.com>
2014-02-24 22:56:51 +0900

    ListViewを一つ表示

--- src/com/example/liquidlists/MainActivity.java
+++ src/com/example/liquidlists/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.liquidlists;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.item_layout);
 	}
 
 	@Override
