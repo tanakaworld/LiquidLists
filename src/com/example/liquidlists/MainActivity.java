0fcf3f950e96e8c8808413aa6dda2e91a6259234
yutaro <y.tanaka.kcbbf@gmail.com>
2014-02-24 23:46:35 +0900

    複数パネルへの対応を追加

--- src/com/example/liquidlists/MainActivity.java
+++ src/com/example/liquidlists/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.liquidlists;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+	private static final int ITEMCOUNT = 20;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.item_layout);
+
+		ListView list = (ListView) findViewById(R.id.listView);
+		ArrayList<String> labelList = new ArrayList<String>();
+
+		String label = getString(R.string.item_label);
+
+		for (int i = 1; i <= ITEMCOUNT; i++) {
+			labelList.add(label + i);
+			System.out.println(i);
+		}
+
+		CustomAdapter mAdapter = new CustomAdapter(this, 0, labelList);
+		list.setAdapter(mAdapter);
+
+		list.setDivider(null);
 	}
 }
